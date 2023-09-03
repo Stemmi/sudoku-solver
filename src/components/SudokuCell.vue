77afaa2c8c7723f490653b566fe5d27b9aaa7cb7
Christian Stemmann <stemmi@sanktemmi.de>
2023-09-03 18:26:43 +0200

    feat: pre-filled table with selection

--- src/components/SudokuCell.vue
+++ src/components/SudokuCell.vue
@@ -1,18 +1,31 @@
 <template>
-    <td>
-        
-    {{ display }}
+    <td @click="selected=true">
+        <input
+            v-if="selected"
+            type="text"
+            size="1"
+            @vue:mounted="({ el }) => el.focus()"
+        >
+    {{ value }}
 
     </td>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                selected: false
+            }
+        },
         props: [
             "cell"
         ],
+        methods: {
+
+        },
         computed: {
-            display() {
+            value() {
                 if (!Number.isInteger(+this.cell)) return;
                 else if (this.cell >= 1 && this.cell <= 9) return this.cell;
                 else return;
@@ -24,6 +37,11 @@
 
 
 <style>
-
+    input {
+        font-size: 1.5em;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+    }
 
 </style>
